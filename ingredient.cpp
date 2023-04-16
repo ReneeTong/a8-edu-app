02e386a48d75f2c64de5304d5a6727b83d66f527
Andy Duong <phucdv3011@gmail.com>
2023-04-15 18:16:58 -0600

    Added ingredient class with getter methods

--- ingredient.cpp
+++ ingredient.cpp
@@ -1,9 +1,30 @@
 
 #include "ingredient.h"
 
-ingredient::ingredient(QObject *parent)
-    : QObject{parent}
-{
+ingredient::ingredient(std::string name, std::string category, std::string color, std::string image, std::vector<std::string> tags, b2BodyDef box2ddef)
+    : name(name), category(category), color(color), image(image), tags(tags), box2ddef(box2ddef) {}
 
+//All getter methods:
+std::string ingredient::getName() const {
+    return name;
 }
 
+std::string ingredient::getCategory() const {
+    return category;
+}
+
+std::string ingredient::getColor() const {
+    return color;
+}
+
+std::string ingredient::getImage() const {
+    return image;
+}
+
+std::vector<std::string> ingredient::getTags() const {
+    return tags;
+}
+
+b2BodyDef ingredient::getBox2dDef() const {
+    return box2ddef;
+}
--- ingredient.h
+++ ingredient.h
@@ -2,18 +2,31 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
 
+#include <string>
+#include <vector>
+#include <Box2D/Box2D.h>
 
-#include <QObject>
 
-
-class ingredient : public QObject
+class ingredient
 {
-    Q_OBJECT
 public:
-    explicit ingredient(QObject *parent = nullptr);
+    ingredient(std::string name, std::string category, std::string color, std::string image, std::vector<std::string> tags, b2BodyDef box2ddef );
 
-signals:
+    // Getter methods
+    std::string getName() const;
+    std::string getCategory() const;
+    std::string getColor() const;
+    std::string getImage() const;
+    std::vector<std::string> getTags() const;
+    b2BodyDef getBox2dDef() const;
 
+private:
+    std::string name;
+    std::string category;
+    std::string color;
+    std::string image;
+    std::vector<std::string> tags;
+    b2BodyDef box2ddef;
 };
 
 #endif // INGREDIENT_H
