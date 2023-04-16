5d195a902d68a6813545be440f52a42be2273bdd
jeffohh <jeffreyle20@gmail.com>
2023-04-15 22:40:47 -0600

    Created ScrollCarousel class
    - scroll area abstraction
    - smooth scrolling with gestures

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -3,6 +3,8 @@
 #include "s2Recipe.h"
 #include "ui_MainWindow.h"
 
+#include <QStackedWidget>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       ui(new Ui::MainWindow)
@@ -10,27 +12,30 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->widget->show();
 
+    QStackedWidget *stackedWidget = new QStackedWidget;
+    setCentralWidget(stackedWidget);
+
     page1 = new s1Pantry(this);
     page2 = new s2Recipe(this);
     page3 = new s3Cooking(this);
     page4 = new s4Complete(this);
-    ui->stackedWidget->addWidget(page1);
-    ui->stackedWidget->addWidget(page2);
-    ui->stackedWidget->addWidget(page3);
-    ui->stackedWidget->addWidget(page4);
-    ui->stackedWidget->setCurrentWidget(page1);
+    stackedWidget->addWidget(page1);
+    stackedWidget->addWidget(page2);
+    stackedWidget->addWidget(page3);
+    stackedWidget->addWidget(page4);
+    stackedWidget->setCurrentWidget(page1);
 
     connect(page1, &s1Pantry::goToPage2, this, [=]() {
-        ui->stackedWidget->setCurrentWidget(page2);
+        stackedWidget->setCurrentWidget(page2);
     });
     connect(page2, &s2Recipe::goToPage3, this, [=]() {
-        ui->stackedWidget->setCurrentWidget(page3);
+        stackedWidget->setCurrentWidget(page3);
     });
     connect(page3, &s3Cooking::goToPage4, this, [=]() {
-        ui->stackedWidget->setCurrentWidget(page4);
+        stackedWidget->setCurrentWidget(page4);
     });
     connect(page4, &s4Complete::goToPage1, this, [=]() {
-        ui->stackedWidget->setCurrentWidget(page1);
+        stackedWidget->setCurrentWidget(page1);
     });
 }
 
