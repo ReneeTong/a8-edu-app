d327e1f94577737314a5d6bf66919b74876e6297
ReneeTong <35294703+ReneeTong@users.noreply.github.com>
2023-04-19 17:56:44 -0600

    add comments

--- main.cpp
+++ main.cpp
@@ -8,10 +8,8 @@ int main(int argc, char *argv[])
     Model model;
     MainWindow w(model);
 
-    // Create a QIcon object with the path to your icon file
+    //set app icon
     QIcon icon(":/sprites/icons/Kitchen.PNG");
-
-    // Set the application icon using the setWindowIcon method
     a.setWindowIcon(icon);
 
     w.show();
