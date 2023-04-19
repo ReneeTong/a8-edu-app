58f48439c9f0283ef1a469052a35a28a8d3d636f
ReneeTong <35294703+ReneeTong@users.noreply.github.com>
2023-04-18 23:51:44 -0600

    add cut effect, able to tell which object is which

---
+++ box2d.cpp
@@ -0,0 +1,7 @@
+#include "box2d.h"
+
+box2d::box2d(QWidget *parent)
+    : QWidget{parent}
+{
+    this->setStyleSheet("background-color: red");
+}
--- mycontactlistener.h
+++ mycontactlistener.h
@@ -4,16 +4,23 @@
 
 #include <Box2D/Box2D.h>
 #include <vector>
+#include "QtCore/qobject.h"
 #include "box.h"
 
-class MyContactListener : public b2ContactListener {
+class MyContactListener :public QObject,  public b2ContactListener
+{
+    Q_OBJECT
 public:
-    MyContactListener(std::vector<Box>& boxes);
+    MyContactListener(QMap<std::string, Box*>&  boxes);
     void BeginContact(b2Contact* contact) override;
     void removeImageFromBody(b2Body* body);
 
 private:
-    std::vector<Box>* boxes;
+    //std::vector<Box>* boxes;
+    QMap<std::string, Box*>*  boxes;
+
+signals:
+    void cut(std::string);
 
 };
 #endif // MYCONTACTLISTENER_H
