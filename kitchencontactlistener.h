d59ae6e5b3ba1768e8e53d74bbe615ebeaab7536
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-04-27 10:29:31 -0600

    added comments to kitchencontactlistener.h

--- kitchencontactlistener.h
+++ kitchencontactlistener.h
@@ -3,9 +3,17 @@
 
 #include <Box2D/Box2D.h>
 
+/**
+ * @brief The KitchenContactListener class Represents a event listener to b2 object contact
+ */
 class KitchenContactListener : public b2ContactListener
 {
 public:
+
+    /**
+     * @brief BeginContact 2 b2 objects contact each otherS
+     * @param contact b2Contsct pointer
+     */
     virtual void BeginContact(b2Contact* contact);
 };
 
