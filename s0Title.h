dac208fc3dc88b9fa974d9b07658462ffdc36251
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-04-27 10:56:47 -0600

    Added comments

--- s0Title.h
+++ s0Title.h
@@ -11,19 +11,37 @@ namespace Ui {
 class s0Title;
 }
 
+/**
+ * @brief The s0Title class Represents the welcome page
+ */
 class s0Title : public QWidget
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief s0Title Constructor
+     * @param parent Parents widget
+     */
     explicit s0Title(QWidget *parent = nullptr);
     ~s0Title();
 
 public slots:
+    /**
+     * @brief nextPage Goes to the next page
+     */
     void nextPage();
+
+    /**
+     * @brief aboutPage Goes to the about page
+     */
     void aboutPage();
 
 signals:
+
+    /**
+     * @brief goToPage1 Goes to the pantry page
+     */
     void goToPage1();
 
 
--- scrollcarousel.h
+++ scrollcarousel.h
@@ -23,27 +23,65 @@
 #include <QTimer>
 #include <QScrollerProperties>
 
+/**
+ * @brief The ScrollCarousel class Represent Scroll area
+ */
 class ScrollCarousel : public QScrollArea
 {
     Q_OBJECT
 public:
+
+    /**
+     * @brief ScrollCarousel Constructor
+     * @param parent Parents widget
+     */
     explicit ScrollCarousel(QWidget *parent = nullptr);
+
+    /**
+     * @brief ScrollCarousel Constructor
+     * @param parent parent widget
+     */
     ScrollCarousel(bool, QWidget *parent = nullptr);
 
+    /**
+     * @brief addWidget Adds a widget to scroll area
+     */
     void addWidget(QWidget*);
+
+    /**
+     * @brief clearWidgets Remove widget from scroll area
+     */
     void clearWidgets();
 
+    /**
+     * @brief addFilter Applies a Filter Adds a filter
+     */
     void addFilter(bool (*)(QWidget*));
+
+    /**
+     * @brief removeFilter Removed the filter
+     */
     void removeFilter(bool (*)(QWidget*));
 
+    /**
+     * @brief sortWidgetsBy Sorts the widget by a given delegate
+     */
     void sortWidgetsBy(std::function<bool(QWidget*, QWidget*)>);
 
 
 protected:
+
     void wheelEvent(QWheelEvent *event) override;
 
 private:
+    /**
+     * @brief reset Resets the whole area
+     */
     void reset();
+
+    /**
+     * @brief rearrangeWidgets Reaaranges the widges in scroll area
+     */
     void rearrangeWidgets();
 
 
@@ -55,10 +93,6 @@ private:
 
     QPropertyAnimation *horizontalAnimation;
     QPropertyAnimation *verticalAnimation;
-
-
-
-
 
 };
 
