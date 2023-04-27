e1380b4ff9e7e77aeefeb5a72eea68347a1c16d0
Tingting Zhou <zhou.tingting0401@gmail.com>
2023-04-27 10:27:09 -0600

    Added comments to ingredientbutton.h

--- ingredientbutton.h
+++ ingredientbutton.h
@@ -13,22 +13,56 @@ class IngredientButton : public QPushButton
 {
     Q_OBJECT
 public:
+
+    /**
+     * @brief IngredientButton Constructor
+     * @param parent parent widget
+     */
     IngredientButton(Ingredient, int, QWidget *parent = nullptr);
 
+    /**
+     * @brief getIngredient Getter
+     * @return The ingredient
+     */
     Ingredient getIngredient() const;
+
+    /**
+     * @brief getSelected Getter
+     * @return true, if the ingredient is selcted; otherwise, false
+     */
     bool getSelected() const;
+
+    /**
+     * @brief setSelected Setter
+     */
     void setSelected(bool);
+
+    /**
+     * @brief setSelectable Setter
+     */
     void setSelectable(bool);
 
-    //
+    /**
+     * @brief event Hover events for ingredient buttons
+     * @param event The event
+     * @return true, if it is hoverred; otherwise, false
+     */
     bool event(QEvent *event);
+
 private:
     Ingredient ingredient;
     bool selected;
     bool selectable;
 
 signals:
+    /**
+     * @brief onSelectedListUpdate Updates the selected ingredient list
+     */
     void onSelectedListUpdate(Ingredient);
+
+    /**
+     * @brief onSelected The ingredient is selected
+     */
     void onSelected(bool);
 };
 
