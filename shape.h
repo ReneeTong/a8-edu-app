e5f461047f900a3cae878f108836c0456f799f8e
jeffohh <jeffreyle20@gmail.com>
2023-04-27 09:45:26 -0600

    Document comments for Model and Shape class

--- shape.h
+++ shape.h
@@ -13,15 +13,59 @@ class Shape : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Creates a box shape based on passed parameters.
+     *
+     * Creates a box shape based on the passed position, size, density, and friction.
+     * Also adds it to a static map for easy parsing.
+     *
+     * @param world         the world to create the b2Body
+     * @param position      the position of the b2Body
+     * @param size          the size of the box
+     * @param density       the density
+     * @param friction      the friction
+     * @param parent
+     */
     explicit Shape(b2World *world, const b2Vec2& position, const b2Vec2& size, float32 density = 0.5f, float32 friction = 0.3f, QObject *parent = nullptr);
     ~Shape();
 
+    // contains map of shapes that are in the world
+    // through constructor and deconstructor
     static map<b2Body*, Shape*> shapes;
+
+    /**
+     * @brief Returns the b2Body binded to the shape.
+     *
+     * @return              the b2Body
+     */
     b2Body* getBody() const { return m_body; }
+
+    /**
+     * @brief Returns a pointer.
+     *
+     * @return              the pointer
+     */
     void* getData() { return m_data; }
+
+    /**
+     * @brief Returns the size binded to the shape.
+     *
+     * @return              the b2Vec2 size
+     */
     b2Vec2 getSize() const { return m_size; }
 
+    /**
+     * @brief Sets the shape to either static or dynamic.
+     *
+     * @param value         static if true, dynamic otherwise
+     */
     void setStatic(bool value);
+
+    /**
+     * @brief Sets the pointer.
+     *
+     * @param               the pointer
+     */
     void setData(void* data) { m_data = data; }
 
 private:
